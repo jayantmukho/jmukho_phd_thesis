30ef66f751faf5b3adb60bf0f3a7645cd20ca45c
Jayant Mukhopadhaya <jmukho@stanford.edu>
2021-02-08 02:48:19 -0800

    update few nasa crm plots

--- code/image_gen/nasa_crm/plot_rmse_lhs.m
+++ code/image_gen/nasa_crm/plot_rmse_lhs.m
@@ -1,54 +1,55 @@
 clear variables
-load errors/cl_rsme_error.mat
-plot_options = plotting_options('thesis');
-plot_options.width = 5;
-plot_options.height = 5;
-setup_plots(plot_options);
+load errors/cl_rsme_error_lhs.mat
 
-x_ind = 2:2:length(rsme_mf)/2;
+skip_ind = 3;
 
-figure(4); clf;
+x_ind = 2:skip_ind:length(rsme_mf);
+
+figure(1); clf;
 semilogy(ns(x_ind),rsme_mf(x_ind),ns(x_ind),rsme_1f(x_ind))
 % set(gcf,'position',[520 100 560 540]);
 xlabel('Number of High Fidelity Data Points')
 ylabel('RMSE Error')
 legend('Multi-fidelity','Single Fidelity')
-axis([0 200 .008 .03])
+axis([0 200 .008 .05])
 axis square
 
 
 clear variables
-load errors/cd_rsme_error.mat
+skip_ind = 2;
+load errors/cd_rsme_error_lhs.mat
 
-x_ind = 1:2:length(rsme_mf)/2;
+x_ind = 1:skip_ind:length(rsme_mf);
 
-figure(5); clf;
-semilogy(ns(x_ind),rsme_mf(x_ind),ns(x_ind),rsme_1f(x_ind))
+figure(2); clf;
+semilogy(ns(x_ind),rsme_1f(x_ind),ns(x_ind),rsme_mf(x_ind))
 % set(gcf,'position',[520 100 560 540]);
 xlabel('Number of High Fidelity Data Points')
 ylabel('RMSE Error')
 legend('Multi-fidelity','Single Fidelity')
+axis([0 200 .001 .02])
 axis square
 
 clear variables
-load errors/cm_rsme_error.mat
+skip_ind = 2;
+load errors/cm_rsme_error_lhs.mat
 
-x_ind = 2:2:length(rsme_mf)/2;
+x_ind = 2:skip_ind:length(rsme_mf);
 
-figure(6); clf;
+figure(3); clf;
 semilogy(ns(x_ind),rsme_mf(x_ind),ns(x_ind),rsme_1f(x_ind))
 % set(gcf,'position',[520 100 560 540]);
 xlabel('Number of High Fidelity Data Points')
 ylabel('RMSE Error')
 legend('Multi-fidelity','Single Fidelity')
-axis([0 200 .008 .02])
+axis([0 200 .008 .05])
 axis square
 %%
-figure(4);
-saveas(gcf,'images/cl_2d_rsme_comp','png')
+figure(1);
+saveas(gcf,'images/cl_2d_rsme_lhs_comp','png')
 
-figure(5);
-saveas(gcf,'images/cd_2d_rsme_comp','png')
+figure(2);
+saveas(gcf,'images/cd_2d_rsme_lhs_comp','png')
 
-figure(6);
-saveas(gcf,'images/cm_2d_rsme_comp','png')
+figure(3);
+saveas(gcf,'images/cm_2d_rsme_lhs_comp','png')
